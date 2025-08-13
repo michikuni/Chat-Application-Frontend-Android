3de9de9db6e05127b28a1f1cad2ec2c76a851c44
Minh Phuong Dang <minhphuonglcby@gmail.com>
2025-08-13 14:31:35 +0700

    refactor viewmodel

---
+++ app/src/main/java/com/company/myapplication/viewmodel/ConversationViewModel.kt
@@ -0,0 +1,61 @@
+package com.company.myapplication.viewmodel
+
+import android.app.Activity
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.company.myapplication.data.model.chat.CreateConversation
+import com.company.myapplication.data.model.chat.GetConversation
+import com.company.myapplication.data.model.chat.Message
+import com.company.myapplication.repository.ConversationRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class ConversationViewModel(activity: Activity): ViewModel(){
+    private val repo = ConversationRepository(activity)
+
+    var errorMsg by mutableStateOf<String?>(null)
+
+    private val _messages = MutableStateFlow<List<Message>>(emptyList())
+    val messages: StateFlow<List<Message>> get() = _messages
+
+    private var createConversationSuccess by mutableStateOf(false)
+
+    private val _conversation = MutableStateFlow<List<GetConversation>?>(emptyList())
+    val conversation: StateFlow<List<GetConversation>?> get() = _conversation
+
+    fun getAllMessage(userId: Long, friendId: Long){
+        viewModelScope.launch {
+            try {
+                val message = repo.getAllMessage(userId, friendId)
+                if (message != null){
+                    _messages.value = message
+                }
+            } catch (e: Exception){
+                errorMsg = e.message
+            }
+        }
+    }
+
+    fun createConversation(userId: Long, body: CreateConversation){
+        viewModelScope.launch {
+            val createConversation = repo.createConversation(userId = userId, body = body)
+            createConversationSuccess = createConversation
+        }
+    }
+
+    fun getAllConversation(userId: Long){
+        viewModelScope.launch {
+            try {
+                val conversation = repo.getAllConversation(userId)
+                _conversation.value = conversation
+            } catch (e: Exception){
+                errorMsg = e.message
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/company/myapplication/viewmodel/FriendViewModel.kt
@@ -0,0 +1,103 @@
+package com.company.myapplication.viewmodel
+
+import android.app.Activity
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.company.myapplication.data.model.response.FriendResponse
+import com.company.myapplication.data.model.response.UserResponse
+import com.company.myapplication.repository.FriendRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import okhttp3.RequestBody
+
+class FriendViewModel (activity: Activity): ViewModel(){
+    private val repo = FriendRepository(activity)
+
+    private var errorMsg by mutableStateOf<String?>(null)
+
+    private val _allFriends = MutableStateFlow<List<UserResponse>>(emptyList())
+    val allFriends: StateFlow<List<UserResponse>> = _allFriends
+
+    private var sendAddFriendSuccess by mutableStateOf(false)
+
+    private var acceptedFriendSuccess by mutableStateOf(false)
+
+    private var canceledFriendSuccess by mutableStateOf(false)
+
+    private val _pendingFriends = MutableStateFlow<List<FriendResponse>>(emptyList())
+    val pendingFriends: StateFlow<List<FriendResponse>> get() = _pendingFriends
+
+    private val _requestFriends = MutableStateFlow<List<FriendResponse>>(emptyList())
+    val requestFriends: StateFlow<List<FriendResponse>> get() = _requestFriends
+
+    fun getAllFriendsById(userId: Long){
+        viewModelScope.launch {
+            try {
+                val friends = repo.getAllFriendsById(userId = userId)
+                _allFriends.value = friends
+            } catch (e: Exception){
+                errorMsg = e.message
+            }
+        }
+    }
+
+    fun sendAddRequest(userId: Long, receiverEmail: RequestBody){
+        viewModelScope.launch {
+            try {
+                val sendAdd = repo.sendAddRequest(userId = userId, receiverEmail = receiverEmail)
+                sendAddFriendSuccess = sendAdd
+                errorMsg = if (sendAdd) null else "Gửi lời mời kết bạn thất bại"
+            } catch (e: IllegalArgumentException){
+                errorMsg = e.message
+            }
+        }
+    }
+
+    fun acceptedFriendRequest(friendshipId: Long){
+        viewModelScope.launch {
+            try {
+                val acceptedSuccess = repo.acceptedFriendRequest(friendshipId = friendshipId)
+                acceptedFriendSuccess = acceptedSuccess
+            } catch (e: IllegalArgumentException){
+                errorMsg = e.message
+            }
+        }
+    }
+
+    fun cancelFriendRequest(friendshipId: Long){
+        viewModelScope.launch {
+            try {
+                val cancelFriendSuccess = repo.cancelFriendRequest(friendshipId = friendshipId)
+                canceledFriendSuccess = cancelFriendSuccess
+            } catch (e: IllegalArgumentException){
+                errorMsg = e.message
+            }
+        }
+    }
+
+    fun getPendingFriendRequest(userId: Long){
+        viewModelScope.launch {
+            try {
+                val pendingFriends = repo.getPendingFriends(userId = userId)
+                _pendingFriends.value = pendingFriends
+            } catch (e: Exception){
+                errorMsg = e.message
+            }
+        }
+    }
+
+    fun getRequestFriendRequest(userId: Long){
+        viewModelScope.launch {
+            try {
+                val friendsRequest = repo.getRequestFriends(userId = userId)
+                _requestFriends.value = friendsRequest
+            } catch (e: Exception){
+                errorMsg = e.message
+            }
+        }
+    }
+}
